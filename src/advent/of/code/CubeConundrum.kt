c655909be71c3cb8ce3bfa2263c862bc1d1bb813
medhakant <medhakant13@gmail.com>
2024-03-21 17:55:11 +0530

    solved day 6

--- src/advent/of/code/CubeConundrum.kt
+++ src/advent/of/code/CubeConundrum.kt
@@ -1,15 +1,16 @@
 package advent.of.code
+
 import java.io.File
 
 //https://adventofcode.com/2023/day/1
 
-private fun processString1(record: String,loaded: Map<String,Int>): Int {
+private fun processString1(record: String, loaded: Map<String, Int>): Int {
     val game = Regex("\\d+").find(record.split(": ")[0])?.value?.toInt()
-    for(set in record.trim().split(": ")[1].split("; ") ){
-        for(cubes in set.split(", ")){
+    for (set in record.trim().split(": ")[1].split("; ")) {
+        for (cubes in set.split(", ")) {
             val count = Regex("\\d+").find(cubes)?.value?.toInt()
             val color = Regex("\\D+").find(cubes)?.value?.trim()
-            if(count!! > loaded[color]!!){
+            if (count!! > loaded[color]!!) {
                 return 0
             }
         }
@@ -18,26 +19,25 @@ private fun processString1(record: String,loaded: Map<String,Int>): Int {
 }
 
 private fun processString2(record: String): Int {
-    var loaded = mutableMapOf<String,Int>()
-    val game = Regex("\\d+").find(record.split(": ")[0])?.value?.toInt()
-    for(set in record.trim().split(": ")[1].split("; ") ){
-        for(cubes in set.split(", ")){
+    val loaded = mutableMapOf<String, Int>()
+    for (set in record.trim().split(": ")[1].split("; ")) {
+        for (cubes in set.split(", ")) {
             val count = Regex("\\d+").find(cubes)?.value?.toInt()
             val color = Regex("\\D+").find(cubes)?.value?.trim()
-            if(!loaded.keys.contains(color) || loaded[color]!!<count!!){
-                loaded[color!!]=count!!
+            if (!loaded.keys.contains(color) || loaded[color]!! < count!!) {
+                loaded[color!!] = count!!
             }
         }
     }
-    return loaded.values.reduce{prod,count->prod*count}
+    return loaded.values.reduce { prod, count -> prod * count }
 }
 
 fun main() {
     val filename = "./data/day2.txt"
     // part 1
     try {
-        val loaded= mapOf("red" to 12,"green" to 13,"blue" to 14)
-        val answer = File(filename).readLines().sumOf { record -> processString1(record,loaded) }
+        val loaded = mapOf("red" to 12, "green" to 13, "blue" to 14)
+        val answer = File(filename).readLines().sumOf { record -> processString1(record, loaded) }
         println(answer)
     } catch (e: Exception) {
         print(e.message)
--- src/advent/of/code/GearRatios.kt
+++ src/advent/of/code/GearRatios.kt
@@ -1,72 +1,76 @@
 package advent.of.code
+
 import java.io.File
 
-private fun isPartNumber(row: Int,firstIndex: Int,lastIndex:Int,schematic: List<String>):Boolean{
-  val maxRows = schematic.size
-  val maxCols = schematic[0].length
-  for(i in maxOf(0,row-1)..minOf(maxRows-1,row+1)){
-    for(j in maxOf(0,firstIndex-1)..minOf(maxCols-1,lastIndex+1)){
-        if(!schematic[i][j].isDigit() && schematic[i][j]!='.'){
-          return true
+private fun isPartNumber(row: Int, firstIndex: Int, lastIndex: Int, schematic: List<String>): Boolean {
+    val maxRows = schematic.size
+    val maxCols = schematic[0].length
+    for (i in maxOf(0, row - 1)..minOf(maxRows - 1, row + 1)) {
+        for (j in maxOf(0, firstIndex - 1)..minOf(maxCols - 1, lastIndex + 1)) {
+            if (!schematic[i][j].isDigit() && schematic[i][j] != '.') {
+                return true
+            }
         }
     }
-  }
-  return false
-}
-private fun processSchematic1(schematic: List<String>):Int{
-  var sum=0
-  for(i in 0..<schematic.size){
-    val matches = Regex("\\d+").findAll(schematic[i])
-    for (match in matches){
-      if(isPartNumber(i,match.range.first,match.range.last,schematic)){
-        sum += match.value.toInt()
-      }
-    }
-  }
-  return sum
+    return false
 }
 
-private fun getGearRation(row:Int,col:Int,schematic: List<String>):Int{
-  var gears = mutableListOf<Int>()
-  for(k in maxOf(0,row-1)..minOf(row+1,schematic.size-1)){
-    val matches = Regex("\\d+").findAll(schematic[k])
-    for(match in matches){
-      if(col in match.range.first-1..match.range.last+1){
-        gears.add(match.value.toInt())
-      }
+private fun processSchematic1(schematic: List<String>): Int {
+    var sum = 0
+    for (i in 0..<schematic.size) {
+        val matches = Regex("\\d+").findAll(schematic[i])
+        for (match in matches) {
+            if (isPartNumber(i, match.range.first, match.range.last, schematic)) {
+                sum += match.value.toInt()
+            }
+        }
     }
-  }
-  if(gears.size==2){
-    return gears[0]*gears[1]
-  }
-  return 0
+    return sum
 }
-private fun processSchematic2(schematic: List<String>):Int{
-  var sum=0
-  for(i in 0..<schematic.size){
-    for(j in 0..<schematic[i].length){
-      if(schematic[i][j]=='*'){
-        sum+=getGearRation(i,j,schematic)
-      }
-    }
-  }
-  return sum
-}
-fun main(args: Array<String>) {
-  try {
-    val fileName = "./data/day3.txt"
-    val schematic = File(fileName).readLines()
-    println(processSchematic1(schematic))
-  }catch (e: Exception){
-    println(e.message)
-  }
 
-  try {
-    val fileName = "./data/day3.txt"
-    val schematic = File(fileName).readLines()
-    println(processSchematic2(schematic))
-  }catch (e: Exception){
-    println(e.message)
-  }
+private fun getGearRation(row: Int, col: Int, schematic: List<String>): Int {
+    val gears = mutableListOf<Int>()
+    for (k in maxOf(0, row - 1)..minOf(row + 1, schematic.size - 1)) {
+        val matches = Regex("\\d+").findAll(schematic[k])
+        for (match in matches) {
+            if (col in match.range.first - 1..match.range.last + 1) {
+                gears.add(match.value.toInt())
+            }
+        }
+    }
+    if (gears.size == 2) {
+        return gears[0] * gears[1]
+    }
+    return 0
+}
+
+private fun processSchematic2(schematic: List<String>): Int {
+    var sum = 0
+    for (i in 0..<schematic.size) {
+        for (j in 0..<schematic[i].length) {
+            if (schematic[i][j] == '*') {
+                sum += getGearRation(i, j, schematic)
+            }
+        }
+    }
+    return sum
+}
+
+fun main() {
+    try {
+        val fileName = "./data/day3.txt"
+        val schematic = File(fileName).readLines()
+        println(processSchematic1(schematic))
+    } catch (e: Exception) {
+        println(e.message)
+    }
+
+    try {
+        val fileName = "./data/day3.txt"
+        val schematic = File(fileName).readLines()
+        println(processSchematic2(schematic))
+    } catch (e: Exception) {
+        println(e.message)
+    }
 
 }
